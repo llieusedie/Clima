91a0cf8ef30ba132a204a93dbe301885a54f7ed1
llieusedie <90383958+llieusedie@users.noreply.github.com>
2022-06-22 16:36:25 +0300

    Initial Commit

---
+++ Clima-LondonAppBrewery/Clima_LondonAppBreweryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clima_LondonAppBreweryApp.swift
+//  Clima-LondonAppBrewery
+//
+//  Created by Paul Kirnoz on 22.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct Clima_LondonAppBreweryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
